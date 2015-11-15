11dc7fbcf18da28aee4287c92499a1c031d481af
Leon Cheung <funarchitect2022@gmail.com>
2015-11-15 08:46:55 -0800

    add navigation to to form

--- besserenklangen/subs/views.py
+++ besserenklangen/subs/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from django.http import Http404
 from django.shortcuts import render
+
+from .forms import UserForm
 
 def index(request):
     context = { "message":  request}
@@ -8,3 +11,16 @@ def index(request):
 
 def about(request):
     return render(request, 'subs/about.html')
+
+def track(request):
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+
+        if form.is_valid():
+            
+            
+            return HttpResponseRedirect('track.html')
+    else:
+        form = UserForm()
+
+    return render(request, 'subs/track.html', { 'form': form })
